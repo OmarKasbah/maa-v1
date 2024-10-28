d60511f4c73015506a50d48e5322b34323bd7c5c
Omar Kasbah <omar.kasbah@outlook.de>
2024-10-28 02:44:49 +0100

    addeed base-path

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/maa-v1/',  // Add this line to configure the correct base path
   plugins: [react()],
 })
